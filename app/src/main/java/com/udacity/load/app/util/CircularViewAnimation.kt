6a78cd1b1c9c579be3d8a3dbfa09231fc569e5ba
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-25 17:25:40 -0500

    refactor: Remove logs

--- app/src/main/java/com/udacity/load/app/util/CircularViewAnimation.kt
+++ app/src/main/java/com/udacity/load/app/util/CircularViewAnimation.kt
@@ -1,6 +1,5 @@
 package com.udacity.load.app.util
 
-import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.Transformation
 
@@ -9,10 +8,6 @@ class CircularViewAnimation(
     progress: Float
 ) :
     Animation() {
-
-    init {
-        Log.i("z- circle", "true")
-    }
 
     private var oldAngle: Float = circularView.angle
     private var newAngle = (progress * 360) / 100
