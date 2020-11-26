0ec02cc01512955444f2bd61788f6dde57e3fdf9
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-26 15:11:42 -0500

    feat: Add item model

---
+++ domain/src/main/java/com/udacity/load/app/domain/model/ItemModel.kt
@@ -0,0 +1,7 @@
+package com.udacity.load.app.domain.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ItemModel(val id: Int, val description: String, val url: String) : Parcelable
