2d932374a1da328d54a2902062cb7dd9f63542a1
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-26 17:57:21 -0500

    refactor: Update timeout

--- data/src/main/java/com/udacity/load/app/data/network/ApiManager.kt
+++ data/src/main/java/com/udacity/load/app/data/network/ApiManager.kt
@@ -34,9 +34,9 @@ object ApiManager {
             val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor(progressInterceptor)
                 .addInterceptor(logging)
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .readTimeout(10, TimeUnit.SECONDS)
-                .writeTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
                 .build()
 
             return Retrofit.Builder()
