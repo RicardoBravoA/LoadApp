b2d52dea346d2b734eba5d2f2fbe0a4992a3fadb
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-26 15:53:57 -0500

    feat: Show items in radio group

--- app/src/main/java/com/udacity/load/app/main/MainFragment.kt
+++ app/src/main/java/com/udacity/load/app/main/MainFragment.kt
@@ -3,19 +3,19 @@ package com.udacity.load.app.main
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.udacity.load.app.R
 import com.udacity.load.app.data.listener.ProgressListener
 import com.udacity.load.app.databinding.FragmentMainBinding
-import com.udacity.load.app.util.CircularViewAnimation
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.udacity.load.app.domain.model.ItemModel
+
 
 class MainFragment : Fragment(), ProgressListener {
 
@@ -56,10 +56,34 @@ class MainFragment : Fragment(), ProgressListener {
         }*/
 
         mainViewModel.itemList.observe(viewLifecycleOwner, {
-
+            showData(it)
         })
 
         return binding.root
+    }
+
+    private fun showData(list: List<ItemModel>) {
+
+        list.forEach {
+            val radioButton = RadioButton(context)
+            radioButton.text = it.description
+            val textSize =
+                (resources.getDimension(R.dimen.size_18) / resources.displayMetrics.scaledDensity)
+            radioButton.textSize = textSize
+
+            val params = RadioGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            val margin = requireContext().resources.getDimension(R.dimen.padding_8).toInt()
+
+            params.setMargins(margin, margin, margin, margin)
+            radioButton.layoutParams = params
+
+            binding.selectRadioGroup.addView(radioButton)
+        }
+
     }
 
     override fun load(progress: Int, contentLength: Long) {
