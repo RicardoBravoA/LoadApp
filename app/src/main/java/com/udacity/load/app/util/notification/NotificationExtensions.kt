9f01d4bdb37b6250c762f7ac2507f086d836cb39
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-28 02:45:03 -0500

    feat: Add action to notification

--- app/src/main/java/com/udacity/load/app/util/notification/NotificationExtensions.kt
+++ app/src/main/java/com/udacity/load/app/util/notification/NotificationExtensions.kt
@@ -4,25 +4,29 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import com.udacity.load.app.R
-import com.udacity.load.app.detail.DetailActivity
 import com.udacity.load.app.domain.model.DetailModel
+import com.udacity.load.app.receiver.SnoozeReceiver
 import com.udacity.load.app.util.Constant
 
 fun NotificationManager.sendNotification(detailModel: DetailModel, applicationContext: Context) {
 
-    val contentIntent = Intent(applicationContext, DetailActivity::class.java)
-    contentIntent.putExtra(Constant.DATA, detailModel)
-
-    val contentPendingIntent = PendingIntent.getActivity(
-        applicationContext,
-        Constant.NOTIFICATION_ID,
-        contentIntent,
-        PendingIntent.FLAG_UPDATE_CURRENT
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    val bundle = Bundle()
+    bundle.putParcelable(Constant.DATA, detailModel)
+    snoozeIntent.putExtra(
+        Constant.DATA,
+        bundle
     )
 
-    val notificationStyle = NotificationCompat.InboxStyle()
+    val snoozePendingIntent = PendingIntent.getBroadcast(
+        applicationContext,
+        Constant.REQUEST_CODE,
+        snoozeIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     val builder = NotificationCompat.Builder(
         applicationContext,
@@ -32,11 +36,14 @@ fun NotificationManager.sendNotification(detailModel: DetailModel, applicationCo
             applicationContext
                 .getString(R.string.notification_title)
         )
-        .setContentIntent(contentPendingIntent)
         .setAutoCancel(true)
-        .setStyle(notificationStyle)
         .setContentText(detailModel.notificationDescription)
         .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .addAction(
+            R.drawable.ic_next,
+            applicationContext.getString(R.string.notification_action),
+            snoozePendingIntent
+        )
 
     notify(Constant.NOTIFICATION_ID, builder.build())
 }
