65e1b1493942b398b9e5b94aa4272c7e8e1ce617
Ricardo Bravo <ricardobravo@outlook.com>
2020-11-27 19:31:50 -0500

    refactor: Update main view model

--- app/src/main/java/com/udacity/load/app/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/load/app/main/MainViewModel.kt
@@ -2,9 +2,13 @@ package com.udacity.load.app.main
 
 import android.app.AlarmManager
 import android.app.Application
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.SystemClock
+import androidx.core.app.AlarmManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -14,6 +18,7 @@ import com.udacity.load.app.domain.usecase.DownloadUseCase
 import com.udacity.load.app.domain.util.ResultType
 import com.udacity.load.app.receiver.AlarmReceiver
 import com.udacity.load.app.util.Constant
+import com.udacity.load.app.util.notification.cancelNotifications
 import com.udacity.load.app.util.resources.ResourcesProvider
 import kotlinx.coroutines.launch
 import java.lang.Exception
@@ -43,7 +48,7 @@ class MainViewModel(
         get() = _alarmOn
 
     init {
-        getData(application)
+        getData()
 
         _alarmOn.value = PendingIntent.getBroadcast(
             getApplication(),
@@ -69,6 +74,23 @@ class MainViewModel(
                 )) {
                     is ResultType.Success -> {
                         _success.value = true
+
+                        val triggerTime = SystemClock.elapsedRealtime()
+
+                        val notificationManager =
+                            ContextCompat.getSystemService(
+                                getApplication(),
+                                NotificationManager::class.java
+                            ) as NotificationManager
+                        notificationManager.cancelNotifications()
+
+                        AlarmManagerCompat.setExactAndAllowWhileIdle(
+                            alarmManager,
+                            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            triggerTime,
+                            notifyPendingIntent
+                        )
+
                     }
                     is ResultType.Error -> {
                         _success.value = false
@@ -80,7 +102,7 @@ class MainViewModel(
         }
     }
 
-    private fun getData(context: Context) {
+    private fun getData() {
         viewModelScope.launch {
             val list = mutableListOf<ItemModel>()
 
